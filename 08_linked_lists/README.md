208c67255c5fdd84d6fede2f5336fc13749e0871
Dakota L Martinez <dakotaleemusic@gmail.com>
2022-07-13 10:28:28 -0700

    add starter code for lesson 8

---
+++ 08_linked_lists/README.md
@@ -0,0 +1,49 @@
+# Data Structures and Algorithms - Linked Lists
+- [] Review Big O notation 
+- [] Go over some of the most popular Data Structures for Interviews 
+- [] Review how to break down an algorithm question 
+- [] Implement a linked list
+
+Given an array `[2, 9, 1, 3]` create a custom method/function to sort the array without using a built in sort method such as `.sort`. This can be done in JavaScript or Ruby 
+
+Once done, do it again but using another algorithm. 
+
+## BIG O time complexity 
+>Note: the following video has flashing lights and colors. 
+[Visualization of 24 Sorting Algorithms In 2 Minutes](https://www.youtube.com/watch?v=BeoCbJPuvSE)
+
+### Time Complexity 
+    The amount of time it takes the algo to run.
+
+![Big-O Complexity Chart](https://miro.medium.com/max/1200/1*5ZLci3SuR0zM_QlZOADv8Q.jpeg)
+- [Big-O Complexity Chart](http://bigocheatsheet.com/) by Eric Rowell
+
+ - O(1) Constant 
+ - O(n) Linear time
+ - O(log n) [Logarithmic time](https://www.youtube.com/watch?v=M4ubFru2O80) 
+ - O(n^2) Quadratic time 
+ - O(n^n) Exponential time
+
+ ### Popular DS and A
+ - Arrays 
+ - LinkedList
+ - Hash
+ - Stacks/Queue 
+ - Trees 
+    - Binary 
+ - Graphs 
+
+ ### How to approach a DS and A question
+
+ - Write the question down and confirm your assumptions  
+ - Write out/explain steps for the solution before coding 
+ - Code out solution 
+ - Run test case examples 
+ - Give Big - O
+
+ ### Singly Linked List
+![Singly Linked List](https://media.geeksforgeeks.org/wp-content/cdn-uploads/20200922124319/Singly-Linked-List1.png)
+
+## Resources
+
+- [LinkedList applications](https://www.geeksforgeeks.org/applications-of-linked-list-data-structure/)
---
+++ 08_linked_lists/linked_list.rb
@@ -0,0 +1,42 @@
+# Make 'pry' available in order to use 'binding.pry'
+require 'pry'
+
+# - Write out/explain steps for the solution before coding 
+# - Code out solution 
+# - Run test case examples 
+# - Give Big-O
+
+# Implement a custom LinkedList Class with:
+
+  # 1. A method (add_to_start) that adds a new Node to the beginning of the LinkedList.
+    # Big-O Time Complexity: O(1)
+  # 2. A method (remove_from_start) that removes the Node at the beginning of the LinkedList
+  # 3. A method (add_to_end) that adds a new Node to the end of each LinkedList. If I need to interact with the end of the LinkedList also, would a Singly Linked List or a Doubly Linked List be better?
+    # Big-O Time Complexity: O(n)
+  # 4. A method (print_all) that prints the value of every Node.
+    # Big-O Time Complexity: O(n)
+
+class Node
+  # Set attr_accessor for :value, :next_node
+  # Define 'initialize' to accept 'value' and 'next_node' (default nil)
+end
+
+class LinkedList
+  # Set attr_accessor for :head
+  # Define 'initialize' to set @head = nil
+
+  # Define 'add_to_start' to add a new Node to the beginning of each LinkedList.
+    # Create a new 'Node' using passed 'value' argument
+    # Set 'new_node.next_node' equal to '@head'
+    # Set '@head' equal to 'new_node' and return
+
+  # Define 'add_to_end' to add a new Node to the end of each LinkedList.
+    # Create a new 'Node' using passed 'value' argument
+    # Assign 'current' to equal 'self.head'
+    # While 'current.next_node' is not falsey, set 'current' equal to 'current.next_node'
+    # Otherwise, set 'current.next_node' equal to 'new_node'
+
+  # Define 'print' to output the value of every Node
+    # Assign 'current' to equal 'self.head'
+    # While 'current' is not falsey, output current.value before setting 'current' equal to 'current.next_node'
+end
